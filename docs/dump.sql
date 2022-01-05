04ee2bb434e1cef0a787f47f1bee3c688b5a6d03
martin murillo <root@vps-1955153-x.dattaweb.com>
2022-01-05 16:57:35 -0300

    nada

--- docs/dump.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE Certificado ADD finalizado TINYINT(1) NOT NULL;
-
-
-####para cliclolectivo en certificados
-
-ALTER TABLE Certificado ADD ciclolectivo_id INT DEFAULT NULL;
-ALTER TABLE Certificado ADD CONSTRAINT FK_D17AED2C3166A0C FOREIGN KEY (ciclolectivo_id) REFERENCES Ciclolectivo (id);
-CREATE INDEX IDX_D17AED2C3166A0C ON Certificado (ciclolectivo_id);
-
